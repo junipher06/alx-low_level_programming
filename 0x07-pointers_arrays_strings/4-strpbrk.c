fabd0bb3f2c4898fb9a419439aff63507100b662
Jennifer Oriwoh <jenniferoriwoh50@gmail.com>
2023-08-28 18:45:11 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,43 +1,25 @@
 #include "main.h"
 
 /**
-
  * _strpbrk - Entry point
-
  * @s: input
-
  * @accept: input
-
  * Return: Always 0 (Success)
-
  */
 
 char *_strpbrk(char *s, char *accept)
 
 {
+	int index;
 
-                int k;
-
-
-                while (*s)
-
-                {
-
-                        for (k = 0; accept[k]; k++)
-
-                        {
-
-                        if (*s == accept[k])
-
-                        return (s);
-
-                        }
-
-                s++;
-
-                }
-
-
-        return ('\0');
-
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
 }
