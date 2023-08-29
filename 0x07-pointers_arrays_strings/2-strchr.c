1a38c02b86ac9140fc4079f7ac8a3672059367ed
Jennifer Oriwoh <jenniferoriwoh50@gmail.com>
2023-08-28 17:50:08 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,34 +1,19 @@
 #include "main.h"
 
 /**
-
  * _strchr - Entry point
-
  * @s: input
-
  * @c: input
-
  * Return: Always 0 (Success)
-
  */
-
 char *_strchr(char *s, char c)
-
 {
+	int index;
 
-        int i = 0;
-
-
-        for (; s[i] >= '\0'; i++)
-
-        {
-
-                if (s[i] == c)
-
-                        return (&s[i]);
-
-        }
-
-        return (0);
-
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+	return ('\0');
 }
